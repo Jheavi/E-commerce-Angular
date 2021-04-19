ce33a1c3969be9f16cb12ec0cca3cadf0e9ec235
Jheavi <javierlaso213@hotmail.com>
2021-04-19 12:04:32 +0200

    add http module and main tag to app component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from "./views/product-list/product-list.component";
+import { ItemListComponent } from "./views/item-list/item-list.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ProductListComponent }
+  { path: 'products', component: ItemListComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { PreHeaderComponent } from './components/pre-header/pre-header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductListComponent } from './views/product-list/product-list.component';
+import { ItemListComponent } from './views/item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreHeaderComponent,
-    ProductListComponent
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
