5288bfeaa0e0f8615be3c6dacfb8779037812c19
Jheavi <javierlaso213@hotmail.com>
2021-04-22 18:23:47 +0200

    improve detail view

--- frontend/src/app/views/detail/detail.component.ts
+++ frontend/src/app/views/detail/detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { computed, observable } from 'mobx-angular';
+import { Item } from 'src/app/item';
+import { ItemsService } from 'src/app/items.service';
 
 @Component({
   selector: 'app-detail',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+  @observable item?: Item
+  productName: string = ''
+  isFixedName: boolean = false
+  selectedImage: string = ''
 
-  constructor() { }
+
+  @computed get discount(): number {
+    return this.item && this.item['actual-price']
+    ? Math.round((1 - +this.item['actual-price'] / +this.item['original-price']) * 100)
+    : 0
+  }
+
+  selectImage(image: string) {
+    this.selectedImage = image
+  }
+
+  constructor(
+    private itemsService: ItemsService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.getItem()
+  }
+
+  getItem(): void {
+    this.productName = this.route.snapshot.paramMap.get('productName')?.replace(/-/g, ' ') || ''
+    this.isFixedName = JSON.parse(this.route.snapshot.queryParams['fixed-name'])
+    this.itemsService.getItem(this.productName, this.isFixedName)
+    .subscribe(item => {
+      this.item = item
+    })
   }
 
 }
