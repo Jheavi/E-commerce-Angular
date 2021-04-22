f530e8105c4a6bf6a88728fa2480af6de27ed677
Jheavi <javierlaso213@hotmail.com>
2021-04-22 12:19:56 +0200

    add firstupper pipe

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -9,13 +9,15 @@ import { PreHeaderComponent } from './components/pre-header/pre-header.component
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ItemListComponent } from './views/item-list/item-list.component';
 import { ItemListDetailComponent } from './components/item-list-detail/item-list-detail.component';
+import { FirstupperPipe } from './pipes/firstupper.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreHeaderComponent,
     ItemListComponent,
-    ItemListDetailComponent
+    ItemListDetailComponent,
+    FirstupperPipe
   ],
   imports: [
     BrowserModule,
