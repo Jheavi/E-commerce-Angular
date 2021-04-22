3c21ca518fb4212dbd2a5374e250cad3ac285786
Jheavi <javierlaso213@hotmail.com>
2021-04-22 14:22:32 +0200

    add path detail/productName in angular router

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailComponent } from './views/detail/detail.component';
 import { ItemListComponent } from "./views/item-list/item-list.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', component: ItemListComponent }
+  { path: 'products', component: ItemListComponent },
+  { path: 'detail/:productName', component: DetailComponent },
 ];
 
 @NgModule({
